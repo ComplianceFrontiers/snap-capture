7046bdf0d316006ebb42f20b49e3cd8683032273
ramya72002 <nsriramya7@gmail.com>
2025-02-19 14:30:09 +0530

    done camera

--- src/pages/UserSelection/UserSelection.js
+++ src/pages/UserSelection/UserSelection.js
@@ -1,27 +1,26 @@
 import React, { useState } from "react";
 import { FaArrowLeft } from "react-icons/fa";
- import Visitor from "../visitor/visitor"; // Import UserSelection component
-
+import Signin from "../signin/signin";
 import "./UserSelection.css";
 
 const UserSelection = () => {
-  const [showVisitor, setShowVisitor] = useState(false);
+  const [showSignin, setShowSignin] = useState(false);
 
-  if (showVisitor) {
-    return <Visitor onBack={() => setShowVisitor(false)} />;
+  if (showSignin) {
+    return <Signin />; // Show Signin component when "Back" is clicked
   }
 
   return (
     <div className="selection-container">
       {/* Back Button */}
-      <div className="back-button" onClick={() => window.history.back()}>
+      <div className="back-button" onClick={() => setShowSignin(true)}>
         <FaArrowLeft className="back-icon" /> Back
       </div>
 
       <div className="grid-container">
         <div className="selection-box">Registered Adult</div>
         <div className="selection-box">BOTS V5 Student</div>
-        <div className="selection-box" onClick={() => setShowVisitor(true)}>
+        <div className="selection-box" onClick={() => setShowSignin(true)}>
           Visitor
         </div>
         <div className="selection-box">BOTS IQ Student</div>
