f65cd13df684e598c449571c6b475f501f94216d
ramya72002 <nsriramya7@gmail.com>
2025-02-20 11:01:25 +0530

    done camera

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Signin from './pages/signin/signin';
-import Capturephoto from "./pages/capturePhoto/capturePhoto";
+// import Capturephoto from "./pages/capturePhoto/capturePhoto";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <POlicyAcknowledgement/> */}
     {/* <USerSelection /> */}
-    <Capturephoto />
+    <Signin />
     {/* <App /> */}
   </React.StrictMode>
 );
