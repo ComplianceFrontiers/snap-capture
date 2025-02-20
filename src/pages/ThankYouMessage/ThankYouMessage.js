ef159817a8c1b2e296aa0f0b9ded7a94f1167ccc
ramya72002 <nsriramya7@gmail.com>
2025-02-20 11:08:36 +0530

    done camera

---
+++ src/pages/ThankYouMessage/ThankYouMessage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./ThankYouMessage.css";
+
+const ThankYouMessage = () => {
+  return (
+    <div className="thank-you-container">
+      <h2 className="thank-you-text">Thank you for visiting us.</h2>
+      <p className="thank-you-subtext">Have a nice day! 😊</p>
+    </div>
+  );
+};
+
+export default ThankYouMessage;
